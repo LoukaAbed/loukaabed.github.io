b6233268ecb32f0e938d3765acd47d0ce1bbfcd8
Louka Abed <louka.abed@yahoo.com>
2026-07-07 03:35:47 +0300

    Refactor layout for title and subheader in contact.py

--- contact.py
+++ contact.py
@@ -77,13 +77,14 @@ def contact(name, email, subject, body, attachments_list=None):
 # --- MAIN APP LAYOUT ---
 left, center, right = st.columns([1, 5, 1])
 with center:
-    st.title("Louka Abed")
-    st.subheader("Clinical Data Scientist | AI Translational Medicine & Pharma R&D")
+    with st.container(horizontal_alignment="center"):
+        st.title("Louka Abed")
+        st.subheader("Clinical Data Scientist | AI Translational Medicine & Pharma R&D")
 
-    st.write(
-        """International Medical Graduate (MD) combining clinical and biochemistry domain expertise with MS in Data Science and Mathematics foundations to validate, 
-        audit, model, and extract AI insights—accelerating novel drug discovery and building predictive translational medicine pipelines through a systems approach 
-        that integrates complex healthcare data streams, high-throughput genomic and proteomic registries, and heterogeneous multiomics architectures.""")
+        st.write(
+            """International Medical Graduate (MD) combining clinical and biochemistry domain expertise with MS in Data Science and Mathematics foundations to validate, 
+            audit, model, and extract AI insights—accelerating novel drug discovery and building predictive translational medicine pipelines through a systems approach 
+            that integrates complex healthcare data streams, high-throughput genomic and proteomic registries, and heterogeneous multiomics architectures.""")
 
 st.divider()
 
