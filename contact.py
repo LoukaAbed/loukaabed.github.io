bdd41f6631de883fe3776e3912be0d26f408780c
Louka Abed <louka.abed@yahoo.com>
2026-07-07 21:46:58 +0300

    Fix formatting in contact.py subheader

--- contact.py
+++ contact.py
@@ -72,7 +72,7 @@ def contact(name, email, subject, body, attachments_list=None):
 left, center, right = st.columns([1, 1, 1])
 with center:
     st.title("Louka Abed")
-st.subheader("Clinical Data Scientist | AI Translational Medicine & Pharma R&D")
+    st.subheader("Clinical Data Scientist | AI Translational Medicine & Pharma R&D")
 st.write("""International Medical Graduate (MD) combining clinical and biochemistry domain expertise with MS in Data Science and Mathematics foundations to validate, 
 audit, model, and extract AI insights—accelerating novel drug discovery and building predictive translational medicine pipelines through a systems approach 
 that integrates complex healthcare data streams, high-throughput genomic and proteomic registries, and heterogeneous multiomics architectures.""")
