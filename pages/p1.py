2e7a550eceb9f2fabb6c4fd640f855f137054526
loukaabed <louka.abed@yahoo.com>
2026-07-10 16:56:05 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -15,22 +15,20 @@ url=os.environ.get("NEON_DB_URL")
 
 #establishing bridge to the database
 bridge=create_engine(url)
+connection=bridge.connect()
 
 def safe(file_1):
     if file_1 is not None:
         if "File1Name" not in st.session_state:
             hex_name = uuid.uuid4().hex
-            st.session_state["File1Name"]=f"user_{hex_name}"
+            st.session_state["File1Name"]=f"User_{hex_name}"
+            st.write(hex_name)
             st.write(st.session_state)
 
 
 #let's user upload a csv small 2MB or less file to be stored in the database
 uploaded_csv = st.file_uploader("Upload csv <= 2MB to be stored in the database", type=["csv"])
 safe(uploaded_csv)
-#remove space from name of the file, and storing the full file name with extension and without in two variables
-file_name = uploaded_csv.name.split('.')[0].replace(' ', '_')
-file_nameCSV = file_name + '.csv'
-file1name=st.session_state["File1Name"]
 
 #Let's check the uploaded file for size and security check and prevent malicious code injection into the database.
 if uploaded_csv is not None :
@@ -45,17 +43,15 @@ if uploaded_csv is not None :
     df = pd.read_csv(uploaded_csv)
     df.columns= df.columns.str.strip().str.lower().str.replace(' ', '_')
 
-    if st.session_state.get('SuccessMessage', False):
-        st.stop()   
-    else:
-        #upload file to the database, replace if exists
-        df.to_sql(name=file1name, con=bridge, if_exists='replace', index=False)
-        st.write(f"your {file_nameCSV} file was uploaded successfully to the database")
-        #Returning the first five rows of the uploaded
-        st.write(f"First five rows of the uploaded file:  {file_nameCSV}")
-        sql_query=f"SELECT * FROM {st.session_state['File1Name']} LIMIT 5"
-        result = pd.read_sql(sql_query, con=bridge)
-        st.write(result)
+    #upload file to the database, replace if exists
+    df.to_sql(name=file1name, con=bridge, if_exists='replace', index=False)
+    st.write(f"your {file_nameCSV} file was uploaded successfully to the database")
+
+    #Returning the first five rows of the uploaded
+    st.write(f"First five rows of the uploaded {file_nameCSV}")
+    sql_query=f"SELECT * FROM {file1name} LIMIT 5"
+    result = pd.read_sql(sql_query, con=bridge)
+    st.write(result)
 if 'File1Name' in st.session_state:
     file1name=st.session_state["File1Name"]
     if st.button(f"Delete {file_nameCSV} Completely from the database"):
@@ -64,9 +60,6 @@ if 'File1Name' in st.session_state:
             connection.execute(text(sql_query))
             del st.session_state["File1Name"]
             st.session_state['SuccessMessage']=True
-            st.session_state['File_Deleted']=True
-            st.rerun()
-            
-if 'File1Name' is not in st.session_state:
+if 'File1Name' not in st.session_state: 
     if st.session_state.get('SuccessMessage', False):
         st.success(f"{file_nameCSV} has been completely deleted from the database.")
