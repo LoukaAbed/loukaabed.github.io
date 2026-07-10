c0a1ef14fe6be88330c9dab5fb69bbe8f71997f3
loukaabed <louka.abed@yahoo.com>
2026-07-11 02:17:36 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import os
 from sqlalchemy import create_engine, text
 
@@ -11,7 +12,7 @@ bridge = create_engine(os.environ.get("NEON_DB_URL"), echo=True)
 def db_fetch(query, var_dic=None):
     with bridge.connect() as conn:
         result = conn.execute(text(query), var_dic)
-        return result.fetchall()
+        return pd.DataFrame(result.mappings())
     
 query = "SELECT * FROM bp where gender = :gender and age > :age"
 st.write(db_fetch(query, {"gender": "Male", "age": 25}))
