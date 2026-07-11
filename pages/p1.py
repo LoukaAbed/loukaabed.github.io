80b5293200385dac92054e845096b4c41c85fa8b
loukaabed <louka.abed@yahoo.com>
2026-07-11 03:48:03 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -5,4 +5,8 @@ st.title("Neon Database Connection Test")
 st.divider()
 
 query = "SELECT * FROM bp where gender = :gender and age > :age"
-st.write(db.db_fetch(query, {"gender": "Male", "age": 25}))
+st.write(db.fetch_db(query, {"gender": "Male", "age": 25}))
+
+st.divider()
+min_age, max_age = st.slider("Age", 0.0, 100.0, value=(0.0, 100.0), key="age")
+st.write(f"Selected age range: {min_age} - {max_age}")
--- utils/db_utils.py
+++ utils/db_utils.py
@@ -5,8 +5,12 @@ from sqlalchemy import create_engine, text
 #build connection to the database
 bridge = create_engine(os.environ.get("NEON_DB_URL"), echo=True)
 
-def db_fetch(query, var_dic=None):
+def fetch_db(query, query_dic=None):
     with bridge.connect() as conn:
-        result = conn.execute(text(query), var_dic)
+        result = conn.execute(text(query), query_dic)
         return pd.DataFrame(result.mappings())
-    
+
+ def edit_db(query, query_dic=None):
+    with bridge.begin() as conn:
+        result = conn.execute(text(query), query_dic)
+        return pd.DataFrame(result.mappings())   
