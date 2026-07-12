b904c1e0b7b98414649c683fe03c1814867861b1
loukaabed <louka.abed@yahoo.com>
2026-07-12 15:34:11 +0300

    commit

--- pages/p2.py
+++ pages/p2.py
@@ -14,18 +14,18 @@ if st.button('Create New Schema'):
     else:
         st.warning("The input field is empty. Please enter text.")
 
-""""
-with st.form(key='delete_selector', border=False):
-    col1, col2 = st.columns([1, 2], vertical_alignment="center")
-    with col1:
-        bttn = st.form_submit_button("Delete Schema")
-    with col2:
-        selector=st.selectbox("Choose schema to delete", db.inside_db(need='schema'))
 
-if bttn and selector: 
-    st.success(f"Schema: {db.schema_db(schma=selector, need='delete_schema')} was successfuly deleted")
-    st.rerun()
-"""
+# with st.form(key='delete_selector', border=False):
+#     col1, col2 = st.columns([1, 2], vertical_alignment="center")
+#     with col1:
+#         bttn = st.form_submit_button("Delete Schema")
+#     with col2:
+#         selector=st.selectbox("Choose schema to delete", db.inside_db(need='schema'))
+
+# if bttn and selector: 
+#     st.success(f"Schema: {db.schema_db(schma=selector, need='delete_schema')} was successfuly deleted")
+#     st.rerun()
+
 dataset = st.file_uploader("Upload multiple files dataset:", type=None, accept_multiple_files=True, key="dataset_upload")
 if dataset: 
     if st.button('Upload Dataset'):
