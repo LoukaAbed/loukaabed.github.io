0048be9706bd765afbcc9c7caa3182c134e270c8
loukaabed <louka.abed@yahoo.com>
2026-07-13 05:34:36 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -21,12 +21,12 @@ st.subheader("User upload file into database, read data, then delete. Will apply
 if 'saved_tables' not in st.session_state:
     st.session_state['saved_tables']=None
     st.session_state['files_count']=0
-if 'uploaded' not in st.session_state:
-    st.session_state['uploaded']={}
+if 'dataset_key' not in st.session_state:
+    st.session_state['dataset_key']={}
 
 
 #using form to prevent db write from random clicks
-with st.form('batch_files_upload', clear_on_submit=False, key='uploaded'):
+with st.form('batch_files_upload', clear_on_submit=False):
     dataset = st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key='dataset_key')
     st.session_state['uploaded']=dataset
     upload_button = st.form_submit_button('Upload Dataset')
@@ -40,7 +40,7 @@ if upload_button:
         st.warning('To upload, please add files first to uploader')
     else:
         st.session_state['files_count']=len(dataset)
-        st.session_state['uploaded'] = {}
+        st.session_state['dataset_key'] = {}
         #reset upload button
         st.session_state['saved_tables'] = None
         with st.spinner('Uploading your files to public schema, processing...'):
