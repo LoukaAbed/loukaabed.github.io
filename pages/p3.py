734d513847ad1ed9caf942b2ef537c8977c48411
loukaabed <louka.abed@yahoo.com>
2026-07-13 22:13:02 +0300

    commit

--- pages/p3.py
+++ pages/p3.py
@@ -4,7 +4,8 @@ st.title('Testing Streamlit and code implementation')
 with st.form('batch_files_upload', clear_on_submit=False):
     uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key='dataset')
     upload_button = st.form_submit_button('Upload Dataset')
-st.write(st.session_state['dataset'][0])
+if not st.write(st.session_state['dataset']):
+    st.write('List is empty')
 if st.session_state['dataset']:
     st.write(st.session_state['dataset'][0].name)
     del st.session_state['dataset']
