2b531a8f1008d26efcf6767a95fd9b04361be5cb
loukaabed <louka.abed@yahoo.com>
2026-07-13 05:24:44 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -30,7 +30,7 @@ if st.session_state['uploaded'] is None:
     with st.form('batch_files_upload', clear_on_submit=False, key='uploaded'):
         dataset = st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key='dataset_key')
         st.session_state['uploaded']=dataset
-        uploaded_button = st.form_submit_button('Upload Dataset')
+        upload_button = st.form_submit_button('Upload Dataset')
 
 
 
