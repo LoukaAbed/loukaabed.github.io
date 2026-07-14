1923ff783ba32b314c0f1b3391fd26b5a3bfc731
loukaabed <louka.abed@yahoo.com>
2026-07-15 02:03:25 +0300

    commit

--- utils/ui.py
+++ utils/ui.py
@@ -5,12 +5,12 @@ def upload(session_state):
         session_state['counter']=0
 
     with st.form('batch_files_upload', clear_on_submit=True):
-        uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key=f"upload_{st.session_state['counter']}")
+        uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key=f"upload_{session_state['counter']}")
         upload_button = st.form_submit_button('Upload Dataset')
 
     if upload_button:
         if uploaded:
-            dataset=st.session_state[f"upload_{st.session_state['counter']}"]
+            dataset=session_state[f"upload_{session_state['counter']}"]
             return dataset
             for file in dataset:
                 st.write(f"File: {file.name} was successfully uploaded")
