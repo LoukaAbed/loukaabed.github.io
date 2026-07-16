6c1765b88e678f60c02631b63110e08ae8b777a0
loukaabed <louka.abed@yahoo.com>
2026-07-16 16:58:27 +0300

    commit

--- pages/p3.py
+++ pages/p3.py
@@ -6,7 +6,7 @@ import time
 st.title('Testing Streamlit and code implementation')
 
 dataset = ui.upload()
-for file, df in dataset.items:
+for file, df in dataset.items():
     st.write(file)
     st.write(df.head())
     time.sleep(2)
