6ff285ef0baa11da73d5cb94ea077502b34e8389
loukaabed <louka.abed@yahoo.com>
2026-07-16 05:42:43 +0300

    commit

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.set_page_config(
 # Define page navigation
 contact = st.Page("pages/contact.py", title="Contact", icon="📬", url_path='Contact')
 p1 = st.Page("pages/p1.py", title="Project 1", icon="📈", url_path='Project1')
-#p2 = st.Page("pages/p2.py", title="Project 2", icon="📈", url_path='Project2')
+p2 = st.Page("pages/p2.py", title="Project 2", icon="📈", url_path='Project2')
 p3 = st.Page("pages/p3.py", title="Project 3", icon="📈", url_path='Project3')
 # Run navigation 
-nav = st.navigation([contact, p1, p3])
+nav = st.navigation([contact, p1, p2, p3])
 nav.run()
