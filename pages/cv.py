ae5e355213a76e45ff936dbf6e16fda7f56d741a
loukaabed <louka.abed@yahoo.com>
2026-07-26 09:53:44 +0300

    cv

--- pages/cv.py
+++ pages/cv.py
@@ -2,7 +2,7 @@ import streamlit as st
 import utils.ui as ui
 
 st.header("Louka Abed", divider="green", anchor=False)
-st.write("Address: Malden, MA 02148 | Phone: 617-942-1441 | E-Mail: louka.abed@yahoo.com")
+st.write("Address: Malden, MA 02148 | Phone: 617-942-1441 | E-Mail: contact@loukaabed.com")
 
 st.subheader("Professional Summary", divider="green", anchor=False)
 st.write("""International Medical Graduate (MD) combining clinical and biochemistry domain expertise with MS in Data Science and Mathematics foundations to validate, 
