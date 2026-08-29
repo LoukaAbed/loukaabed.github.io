8c19c7e824838aafd417363286ae007b7527a6a5
loukaabed <louka.abed@yahoo.com>
2026-08-29 09:57:59 +0300

    updates

--- pages/p4.py
+++ pages/p4.py
@@ -3,11 +3,5 @@ import utils.ui as ui
 import pandas as pd
 import time
 
-st.title('Implementing multifile dataset uploader to a target database schema')
-
-ui.upload()
-dataset=st.session_state['dataset_dic']
-for file in dataset:
-    st.write(file)
-    st.write(dataset[file].head())
-
+st.title("🛡️ FDA-Compliant Regulatory Data Pipeline")
+st.subheader("Clinical ETL Engine: Messy EHR to CDISC SDTM Conversion")
